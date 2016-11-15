5255354f590404812a75c85c72a48dfa153ff86e
leonfloch <leonfloch@icloud.com>
2016-11-14 20:27:54 -0500

    dms

---
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/jms/CallCenterMessage.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.losalpes.jms;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ *
+ * @author leonardovalbuenacalderon
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+})
+public class CallCenterMessage implements MessageListener {
+    
+    public CallCenterMessage() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+Laboratorio 6 – Beans de Mensajería
+=====================
+
+##Objetivos
+IImplementar, para la aplicación Muebles de los Alpes, nuevos requerimientos cuya solución requiere el envío, recepción y procesamiento de mensajes apoyado con Message-Driven Beans (MDBs) de la plataforma JEE. Esto incluye la definición de:
+
+Los MDBs requeridos para solucionar los requerimientos propuestos.
+
+El proceso que se debe seguir para efectuar el envío, la recepción y el procesamiento de mensajes asincrónicos.
+
+##Integrantes
+Juan Carlos Albarracin
+
+Leonardo Valbuena
